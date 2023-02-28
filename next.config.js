cb3c45c4cbb8442ec6bef56848c8860b95207fa5
Abhinav Geethan <72859534+abhinavgeethan@users.noreply.github.com>
2023-02-28 15:48:04 +0530

    Test loader fix

--- next.config.js
+++ next.config.js
@@ -73,49 +73,49 @@ module.exports = {
   },
 }
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  },
-  webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    })
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+//   eslint: {
+//     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: securityHeaders,
+//       },
+//     ]
+//   },
+//   webpack: (config, { dev, isServer }) => {
+//     config.module.rules.push({
+//       test: /\.(png|jpe?g|gif|mp4)$/i,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {
+//             publicPath: '/_next',
+//             name: 'static/media/[name].[hash].[ext]',
+//           },
+//         },
+//       ],
+//     })
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     })
 
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
+//     if (!dev && !isServer) {
+//       // Replace React with Preact only in client production build
+//       Object.assign(config.resolve.alias, {
+//         'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+//         react: 'preact/compat',
+//         'react-dom/test-utils': 'preact/test-utils',
+//         'react-dom': 'preact/compat',
+//       })
+//     }
 
-    return config
-  },
-})
+//     return config
+//   },
+// })
