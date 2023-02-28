926038a0aa8bda6bb8330dcdebf6cf7a81e7eb46
Abhinav Geethan <72859534+abhinavgeethan@users.noreply.github.com>
2023-02-28 15:13:47 +0530

    Add GitHub Pages Config

--- next.config.js
+++ next.config.js
@@ -52,6 +52,18 @@ const securityHeaders = [
   },
 ]
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
